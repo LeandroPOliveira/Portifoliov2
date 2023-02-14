31e395d5b5fc6e441e7e4ce789cce8c4877e2227
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-02-13 21:51:26 -0300

    Ajustes
    - Portfolio
    - configuração inicial envio de email

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
+import os
 from flask import Flask, render_template
+from flask_mail import Mail, Message
 
 app = Flask(__name__)
+app.secret_key = 'lambreta'
+
+mail_settings = {
+    "MAIL_SERVER": 'smtp.gmail.com',
+    "MAIL_PORT": 465,
+    "MAIL_USE_TLS": False,
+    "MAIL_USE_SSL": True,
+    "MAIL_USERNAME": os.environ['user'],
+    "MAIL_PASSWORD": os.environ['senha']
+}
 
 @app.route('/')
 def index():
