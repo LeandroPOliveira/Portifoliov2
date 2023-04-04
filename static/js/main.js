26122c3eca3c89d8d9938edd9a6ef21a59b51e67
Leandro Oliveira <leandro_lpo2@hotmail.com>
2023-04-03 21:58:54 -0300

    contato css js

--- static/js/main.js
+++ static/js/main.js
@@ -132,6 +132,19 @@ function scrollUp(){
   }
   window.addEventListener('scroll', scrollUp)
 
+/*===================== LOAD BUTTON ======================*/
+
+const btnEnviar = document.querySelector('#btn-enviar')
+const btnEnviarLoader = document.querySelector('#btn-enviar-loader')
+
+btnEnviar.addEventListener('click', ()=> {
+    btnEnviarLoader.style.display = 'block';
+    btnEnviar.style.display = "none"
+})
+
+setTimeout(() => {
+    document.querySelector('#alerta').style.display = 'none'
+}, 5000)
 
 /*==================== DARK LIGHT THEME ====================*/ 
 
